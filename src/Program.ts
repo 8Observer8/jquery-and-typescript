6535df15c9b6e69b128b1a5d4e5a51de24f7e2c0
Ivan Enzhaev <8observer8@gmail.com>
2019-12-31 00:37:26 +0400

    Added a link to GitHub

--- src/Program.ts
+++ src/Program.ts
@@ -1,6 +1,7 @@
 import * as $ from "jquery";
 
 // Playground: https://next.plnkr.co/edit/CoBEcB8vlZfQqAfz?preview
+// GitHub: https://github.com/8Observer8/jquery-and-typescript
 
 class Program
 {
